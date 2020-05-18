92dcc13088f52acc0240701c1c68d964b8587860
Casper Boone <mail@casperboone.nl>
2020-05-18 22:28:49 +0200

    Add events

---
+++ database/factories/EventFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Event;
+use Faker\Generator as Faker;
+
+$factory->define(Event::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'capacity' => $faker->randomNumber(2),
+    ];
+});
