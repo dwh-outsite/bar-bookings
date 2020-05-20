75b10a3777e3a7c676ec4d7e5c222aa579663720
Casper Boone <mail@casperboone.nl>
2020-05-20 11:05:19 +0200

    Send cancelation e-mail

--- app/Http/Controllers/Api/CancelBookingController.php
+++ app/Http/Controllers/Api/CancelBookingController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers\Api;
 
 use App\Booking;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Mail\BookingCanceled;
+use Illuminate\Support\Facades\Mail;
 
 class CancelBookingController extends Controller
 {
@@ -13,6 +14,8 @@ class CancelBookingController extends Controller
         $booking = Booking::where('cancelation_token', $token)->firstOrFail();
 
         $booking->cancel();
+
+        Mail::to($booking->email)->queue(new BookingCanceled($booking));
 
         return redirect(config('app.cancelation_redirect_url'));
     }
