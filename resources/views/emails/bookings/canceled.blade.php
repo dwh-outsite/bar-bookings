0dfe4777d1ddd2bcba9825f9472cc392155148a8
Casper Boone <mail@casperboone.nl>
2020-05-30 15:20:47 +0200

    Update cancelation e-mail text
    No longer always per the user's request

--- resources/views/emails/bookings/canceled.blade.php
+++ resources/views/emails/bookings/canceled.blade.php
@@ -1,8 +1,10 @@
 @component('mail::message')
 # Reservering geannuleerd / Booking canceled
 
-Je reservering voor **{{ $booking->event->name }}** op **{{ $booking->event->start  }}** is geannuleerd op jouw verzoek.
+Je reservering voor **{{ $booking->event->name }}** op **{{ $booking->event->start->format('d-m-Y H:i')  }}**.
+Was deze annulering onterecht? Neem alsjeblieft contact op met ons of plaats een nieuwe reservering.
 
-Your booking for **{{ $booking->event->name }}** at **{{ $booking->event->start  }}** has been canceled per your request.
+Your booking for **{{ $booking->event->name }}** at **{{ $booking->event->start->format('d-m-Y H:i')  }}**.
+Did you not mean to cancel your booking? Please contact us or make a new booking.
 
 @endcomponent
