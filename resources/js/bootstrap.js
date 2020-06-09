ea0f68c0d52e05b92a6c5a1d06a71264e0795755
Casper Boone <mail@casperboone.nl>
2020-06-09 21:32:13 +0200

    Add and enable Turbolinks

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -39,3 +39,10 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     forceTLS: true
 // });
+
+/**
+ * Load and enable Turbolinks
+ */
+const Turbolinks = require("turbolinks")
+
+Turbolinks.start()
