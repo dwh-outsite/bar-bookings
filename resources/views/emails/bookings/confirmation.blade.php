d037118e657c3be074abb71366e8b0df11563dba
Casper Boone <mail@casperboone.nl>
2020-06-09 23:15:09 +0200

    Fix email template

--- resources/views/emails/bookings/confirmation.blade.php
+++ resources/views/emails/bookings/confirmation.blade.php
@@ -5,13 +5,13 @@ Je hebt gereserveerd voor **{{ $booking->event->name }}** op **{{ $booking->even
 **{{ $booking->event->start->format('H:i')  }}** uur.
 @if($booking->twoseat)
 Je hebt aangegeven dat je met **twee personen** komt en dat jullie samen binnen 1,5 meter mogen zijn.
-@elseif
+@endif
 
 You succesfully made a booking for **{{ $booking->event->name }}** on **{{ $booking->event->start->format('d-m-Y')  }}**
 at **{{ $booking->event->start->format('H:i')  }}** uur.
 @if($booking->twoseat)
-You have indicated that you are coming with **two person** and that you are allowed to be together within 1.5 meter.
-@elseif
+You have indicated that you are coming with **two persons** and that you are allowed to be together within 1.5 meter.
+@endif
 
 @component('mail::panel')
 
