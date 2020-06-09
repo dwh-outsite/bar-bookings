cc2f1506231db41c9d4106753e2dfb3b7583093d
Casper Boone <mail@casperboone.nl>
2020-06-09 21:19:35 +0200

    Fix bar redirect

--- routes/bar.php
+++ routes/bar.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\Bar\EventsController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(BarAuthentication::class)->group(function () {
-    Route::redirect('', 'bar/events');
+    Route::redirect('', '/bar/events');
     Route::get('events', [EventsController::class, 'index'])->name('bar.events.index');
     Route::get('events/{event}', [EventsController::class, 'show'])->name('bar.events.show');
 });
