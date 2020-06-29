92cbd32eb8a78a1ce8fad1ded07ba594e26cfa8f
Casper Boone <mail@casperboone.nl>
2020-06-29 21:08:17 +0200

    Add placeholders for missing diets/teams

--- resources/views/widgets/dinner-statistics.blade.php
+++ resources/views/widgets/dinner-statistics.blade.php
@@ -5,9 +5,9 @@
             Diets
         </div>
 
-        <div class="w-full p-6">
-            <div class="flex flex-wrap -m-1">
-                @foreach ($diets as $diet => $count)
+        <div class="w-full h-full p-6">
+            <div class="h-full flex flex-wrap -m-1">
+                @forelse ($diets as $diet => $count)
                     <div class="w-1/2 md:w-1/4 my-1">
                         <div class="flex flex-col justify-between bg-purple-200 rounded p-3 mx-1 h-full">
                             <div class="font-semibold text-purple-600 text-sm tracking-wider uppercase mb-3">
@@ -18,7 +18,11 @@
                             </div>
                         </div>
                     </div>
-                @endforeach
+                @empty
+                    <div class="w-full h-full flex items-center justify-center uppercase text-sm tracking-wide text-gray-600 m-1 py-6">
+                        No diet restrictions have been indicated yet
+                    </div>
+                @endforelse
             </div>
         </div>
     </div>
@@ -28,13 +32,17 @@
             Teams
         </div>
 
-        <div class="w-full p-6 leading-loose">
-            @foreach ($teams as $team => $count)
-                <div class="flex">
+        <div class="h-full w-full p-6">
+            @forelse ($teams as $team => $count)
+                <div class="flex leading-loose">
                     <div class="w-10">{{ $count }}</div>
                     <div class="font-bold">{{ $team }}</div>
                 </div>
-            @endforeach
+            @empty
+                <div class="h-full w-full flex items-center justify-center uppercase text-sm tracking-wide text-gray-600 py-6">
+                    No team preferences have been indicated yet
+                </div>
+            @endforelse
         </div>
     </div>
 
