89580d260637f3b24bb2460fce744f50370d3c37
Casper Boone <mail@casperboone.nl>
2020-08-28 15:08:32 +0200

    Re-introduce cache busting for JS assets

--- resources/views/layouts/bar.blade.php
+++ resources/views/layouts/bar.blade.php
@@ -10,7 +10,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="/js/app.js" defer></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
