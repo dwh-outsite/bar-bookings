4b9792f7a05b9a00198858ebcaf77ba3b87aa857
Casper Boone <mail@casperboone.nl>
2020-09-06 19:27:31 +0200

    Filter "left"-bookings in canceled section
    Fixes #11

--- resources/views/bar/events/show.blade.php
+++ resources/views/bar/events/show.blade.php
@@ -40,7 +40,7 @@
             </div>
             <div class="w-1/3 pl-2 h-full flex flex-col space-y-4">
                 <livewire:interactive-bookings title="Left Bookings" :event="$event" :filter-left="true" />
-                <livewire:interactive-bookings title="Canceled Bookings" :event="$event" :filter-active="false" />
+                <livewire:interactive-bookings title="Canceled Bookings" :event="$event" :filter-left="false" :filter-active="false" />
             </div>
         </div>
 
