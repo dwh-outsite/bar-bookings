1754b1fc41574450fccd6fa1b7eedf2e8b6b11f5
Casper Boone <mail@casperboone.nl>
2020-09-10 16:59:25 +0200

    Log IP addresses

--- app/Http/Controllers/Api/RetrieveEventsController.php
+++ app/Http/Controllers/Api/RetrieveEventsController.php
@@ -5,12 +5,15 @@ namespace App\Http\Controllers\Api;
 use App\Event;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Event as EventResource;
+use App\Log;
 use Illuminate\Http\Request;
 
 class RetrieveEventsController extends Controller
 {
     public function __invoke(Request $request)
     {
+        Log::create(['ip' => $request->ip()]);
+
         return EventResource::collection(
             Event::endDateInTheFuture()
                 ->when($request->has('event_type_id'), fn ($query) => $query->where('event_type_id', $request->event_type_id))
