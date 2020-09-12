d40c1b035cd4c93a189efb295ac86607c9c5a515
Casper Boone <mail@casperboone.nl>
2020-09-12 14:23:59 +0200

    Improve styling of new guest button

--- resources/views/livewire/register-new-guest-button.blade.php
+++ resources/views/livewire/register-new-guest-button.blade.php
@@ -1,5 +1,13 @@
 <div>
-    <button wire:click="register" class="font-bold py-4 px-6 text-lg rounded-lg text-gray-100 bg-purple-500 hover:bg-purple-700">
+    <button
+        wire:click="register"
+        class="
+            font-bold py-4 px-6 rounded-full text-gray-100 bg-purple-500 hover:bg-purple-700
+            uppercase tracking-wider
+            inline-flex items-center
+        "
+    >
+        <x-heroicon-o-user-add class="w-5 h-5 mr-2" />
         Register new guest
     </button>
 </div>
