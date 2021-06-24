46a6e3fe49184b8a6ea1ddf104c08c214a9a405d
Casper Boone <mail@casperboone.nl>
2021-06-24 11:54:22 +0200

    Schedule booking reminders command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Console\Commands\AnonymizeOldBookings;
+use App\Console\Commands\SendBookingReminders;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -26,6 +27,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
          $schedule->command(AnonymizeOldBookings::class)->daily();
+         $schedule->command(SendBookingReminders::class)->hourlyAt(5);
     }
 
     /**
